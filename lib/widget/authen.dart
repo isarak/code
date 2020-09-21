29246d334bef9f80466bedf1fca03892c4d7fa65
israk <k.isara@hotmail.com>
2020-09-21 14:34:40 +0700

    redEye

--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Authen extends StatefulWidget {
   //get data push data
@@ -7,15 +8,76 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+
+  bool statusRedEye = true;
+
   //create state
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(width: 250,
-          child: Image.asset('images/logo.png'),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              buildContainer(),
+              buildText(),
+              userText(),
+              passwordText(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Container userText() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      height: 40,
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'User :',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Container passwordText() {
+    return Container(
+      margin: EdgeInsets.only(top: 8),
+      width: 250,
+      height: 40,
+      child: TextField(
+        obscureText: statusRedEye,
+        decoration: InputDecoration(
+            labelText: 'Password :',
+            border: OutlineInputBorder(),
+            suffixIcon:
+                IconButton(icon: Icon(Icons.remove_red_eye), onPressed: (){
+                  setState(() {
+                    statusRedEye = !statusRedEye;
+                  });
+                }),),
+      ),
+    );
+  }
+
+  Text buildText() => Text(
+        'UNPLAN',
+        style: GoogleFonts.montserrat(
+            textStyle: TextStyle(
+                fontSize: 30,
+                color: Colors.blue[300],
+                fontWeight: FontWeight.bold)),
+      );
+
+  Container buildContainer() {
+    return Container(
+      width: 250,
+      child: Image.asset('images/travel.png'),
     );
   }
 }
